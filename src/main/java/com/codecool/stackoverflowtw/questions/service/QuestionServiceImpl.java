7571655644770b5b16c73a693758dc24e1afe93a
Bebe <125598346+SebestyenBenedek@users.noreply.github.com>
2023-10-17 01:22:25 +0200

    implement QuestionServiceImpl class addNewQuestion method

--- src/main/java/com/codecool/stackoverflowtw/questions/service/QuestionServiceImpl.java
+++ src/main/java/com/codecool/stackoverflowtw/questions/service/QuestionServiceImpl.java
@@ -1,7 +1,11 @@
 package com.codecool.stackoverflowtw.questions.service;
 
+import com.codecool.stackoverflowtw.logger.Logger;
+import com.codecool.stackoverflowtw.questions.controller.dto.NewQuestionDTO;
 import com.codecool.stackoverflowtw.questions.dao.QuestionsDAO;
 import com.codecool.stackoverflowtw.questions.controller.dto.QuestionDTO;
+import com.codecool.stackoverflowtw.questions.repository.QuestionRepository;
+import com.codecool.stackoverflowtw.questions.repository.QuestionRepositoryImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,12 +14,20 @@ import java.util.List;
 @Service
 public class QuestionServiceImpl implements QuestionService {
 
+    //private String dbFile;
+    //private Logger logger;
+    private QuestionRepositoryImpl questionRepositoryImpl;
     private QuestionsDAO questionsDAO;
 
     @Autowired
     public QuestionServiceImpl(QuestionsDAO questionsDAO) {
         this.questionsDAO = questionsDAO;
     }
+
+   /* public QuestionServiceImpl(String dbFile, Logger logger) {
+        this.dbFile = dbFile;
+        this.logger = logger;
+    }*/
 
     public List<QuestionDTO> getAllQuestions() {
         // TODO
@@ -33,9 +45,7 @@ public class QuestionServiceImpl implements QuestionService {
         return false;
     }
 
-    public int addNewQuestion(NewQuestionDTO question) {
-        // TODO
-        int createdId = 0;
-        return questionsDAO.;
+    public void addNewQuestion(NewQuestionDTO question) {
+        questionRepositoryImpl.add(question.title(), question.description());
     }
 }
