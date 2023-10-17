c05e2a0b0bfa4947ec79ed4620a12c4eca3d3304
Bebe <125598346+SebestyenBenedek@users.noreply.github.com>
2023-10-17 18:20:35 +0200

    implement UserController class

---
+++ src/main/java/com/codecool/stackoverflowtw/users/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.codecool.stackoverflowtw.users.controller;
+
+import com.codecool.stackoverflowtw.logger.Logger;
+import com.codecool.stackoverflowtw.users.controller.dto.UserDTO;
+import com.codecool.stackoverflowtw.users.service.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    private final UserServiceImpl userService;
+    private Logger logger;
+
+    @Autowired
+    public UserController(UserServiceImpl userService) {
+        this.userService = userService;
+    }
+}
