2d142b31a6417c4baa36f4ddf625419fcc45721c
Bebe <125598346+SebestyenBenedek@users.noreply.github.com>
2023-10-17 22:03:30 +0200

    refactor QuestionDTO record

--- src/main/java/com/codecool/stackoverflowtw/questions/controller/dto/QuestionDTO.java
+++ src/main/java/com/codecool/stackoverflowtw/questions/controller/dto/QuestionDTO.java
@@ -1,5 +1,9 @@
 package com.codecool.stackoverflowtw.questions.controller.dto;
 
-import java.time.LocalDateTime;
+import com.codecool.stackoverflowtw.questions.model.Tag;
 
-public record QuestionDTO(int id, String title, String description, LocalDateTime created) {}
+import java.time.LocalDateTime;
+import java.util.Set;
+
+public record QuestionDTO(int id, String title, String description, LocalDateTime created,
+                          Set<Tag> tagSet, int numberOfAnswer, int numberOfViews) {}
