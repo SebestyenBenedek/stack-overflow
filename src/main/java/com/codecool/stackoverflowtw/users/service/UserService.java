78ee11d1ee0fc66de9fafc4cd373b874f1b39c4f
Bebe <125598346+SebestyenBenedek@users.noreply.github.com>
2023-10-17 17:54:22 +0200

    implement UserService interface

---
+++ src/main/java/com/codecool/stackoverflowtw/users/service/UserService.java
@@ -0,0 +1,15 @@
+package com.codecool.stackoverflowtw.users.service;
+
+import com.codecool.stackoverflowtw.users.controller.dto.NewUserDTO;
+import com.codecool.stackoverflowtw.users.model.User;
+
+import java.util.Set;
+import java.util.UUID;
+
+public interface UserService {
+    Set<User> getAllUsers();
+    User getUserById(UUID id);
+    void deleteUserById(UUID id);
+    void addNewUser(NewUserDTO userDTO);
+    void updateUserById(UUID id);
+}
