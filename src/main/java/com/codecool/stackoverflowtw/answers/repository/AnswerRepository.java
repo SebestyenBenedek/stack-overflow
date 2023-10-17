db0310201d04e84b149832bcb2bdfdcfe9950a20
Bebe <125598346+SebestyenBenedek@users.noreply.github.com>
2023-10-17 22:18:17 +0200

    refactor AnswerRepository interface getAll method parameter

--- src/main/java/com/codecool/stackoverflowtw/answers/repository/AnswerRepository.java
+++ src/main/java/com/codecool/stackoverflowtw/answers/repository/AnswerRepository.java
@@ -5,7 +5,7 @@ import com.codecool.stackoverflowtw.answers.model.Answer;
 import java.util.List;
 
 public interface AnswerRepository {
-    List<Answer> getAll();
+    List<Answer> getAll(int id);
     Answer get(int id);
     void delete(int id);
     void add(String description);
