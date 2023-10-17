a536a9364078464d263b515c195a5d85504910a0
Bebe <125598346+SebestyenBenedek@users.noreply.github.com>
2023-10-17 18:00:11 +0200

    refactor UserService interface

--- src/main/java/com/codecool/stackoverflowtw/users/service/UserService.java
+++ src/main/java/com/codecool/stackoverflowtw/users/service/UserService.java
@@ -1,14 +1,15 @@
 package com.codecool.stackoverflowtw.users.service;
 
 import com.codecool.stackoverflowtw.users.controller.dto.NewUserDTO;
+import com.codecool.stackoverflowtw.users.controller.dto.UserDTO;
 import com.codecool.stackoverflowtw.users.model.User;
 
 import java.util.Set;
 import java.util.UUID;
 
 public interface UserService {
-    Set<User> getAllUsers();
-    User getUserById(UUID id);
+    Set<UserDTO> getAllUsers();
+    UserDTO getUserById(UUID id);
     void deleteUserById(UUID id);
     void addNewUser(NewUserDTO userDTO);
     void updateUserById(UUID id);
