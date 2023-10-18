0f95cf913accc08d12568d3b8446902bf843acf3
Lajos Savoly <125366427+SavolyLali@users.noreply.github.com>
2023-10-18 09:15:37 +0200

    start creating test cases for endpoints

--- src/main/java/com/codecool/stackoverflowtw/questions/dao/QuestionsDaoJdbc.java
+++ src/main/java/com/codecool/stackoverflowtw/questions/dao/QuestionsDaoJdbc.java
@@ -1,7 +1,7 @@
 package com.codecool.stackoverflowtw.questions.dao;
 
 public class QuestionsDaoJdbc implements QuestionsDAO {
-    @Override
+
     public void sayHi() {
         System.out.println("Hi DAO!");
     }
---
+++ src/test/java/com/codecool/stackoverflowtw/questions/service/QuestionServiceImplTest.java
@@ -0,0 +1,29 @@
+package com.codecool.stackoverflowtw.questions.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class QuestionServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getAllQuestions() {
+    }
+
+    @Test
+    void getQuestionById() {
+    }
+
+    @Test
+    void deleteQuestionById() {
+    }
+
+    @Test
+    void addNewQuestion() {
+    }
+}
