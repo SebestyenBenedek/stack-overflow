c1aaba573bd113e41d17c5b2e1bb105d8aa4bb08
Adam Dulai <105586170+Amdu94@users.noreply.github.com>
2023-10-18 09:54:36 +0200

    Create header container.

---
+++ src/webapp/src/pages/UserPage/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './Main.css'
+
+
+const Header = () => {
+
+        return (
+            <div id="home">
+                <h1 className="header-h1">Profile Page</h1>
+            </div>
+        )
+}
+
+export default Header
