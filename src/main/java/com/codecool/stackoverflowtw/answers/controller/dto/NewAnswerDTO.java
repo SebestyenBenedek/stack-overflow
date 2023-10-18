821bca8d3622fb6544bc062f31204ee7ab78759a
Bebe <125598346+SebestyenBenedek@users.noreply.github.com>
2023-10-18 16:33:24 +0200

    refactor NewAnswerDTO record

--- src/main/java/com/codecool/stackoverflowtw/answers/controller/dto/NewAnswerDTO.java
+++ src/main/java/com/codecool/stackoverflowtw/answers/controller/dto/NewAnswerDTO.java
@@ -1,4 +1,4 @@
 package com.codecool.stackoverflowtw.answers.controller.dto;
 
-public record NewAnswerDTO(String description, int questionId) {
+public record NewAnswerDTO(String description, int questionId, int userId) {
 }
