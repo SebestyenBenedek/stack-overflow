d6c9f4106baf057fb129d0e9854123753fb3048c
Bebe <125598346+SebestyenBenedek@users.noreply.github.com>
2023-10-18 16:32:52 +0200

    refactor AnswerService class

--- src/main/java/com/codecool/stackoverflowtw/answers/service/AnswerService.java
+++ src/main/java/com/codecool/stackoverflowtw/answers/service/AnswerService.java
@@ -2,13 +2,60 @@ package com.codecool.stackoverflowtw.answers.service;
 
 import com.codecool.stackoverflowtw.answers.controller.dto.AnswerDTO;
 import com.codecool.stackoverflowtw.answers.controller.dto.NewAnswerDTO;
+import com.codecool.stackoverflowtw.answers.model.Answer;
+import com.codecool.stackoverflowtw.answers.repository.AnswerRepositoryImpl;
+import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.HashSet;
 import java.util.Set;
 
-public interface AnswerService {
-    Set<AnswerDTO> getAllAnswers(int id);
-    AnswerDTO getAnswerById(int id);
-    void deleteAnswerById(int id);
-    void addNewAnswer(NewAnswerDTO answer);
-    void updateAnswerById(int id, AnswerDTO answerDTO);
+@Service
+public class AnswerService {
+    public static final int STARTER_NUMBER_OF_LIKES = 0;
+    public static final int STARTER_NUMBER_OF_DISLIKES = 0;
+    private AnswerRepositoryImpl answerRepository;
+
+    public Set<AnswerDTO> getAllAnswers(int questionId) {
+        Set<Answer> answers = answerRepository.getAll(questionId);
+        Set<AnswerDTO> answerDTOS = new HashSet<>();
+
+        for (Answer answer : answers) {
+            answerDTOS.add(new AnswerDTO(
+                    answer.getId(),
+                    answer.getDescription(),
+                    answer.getQuestionId(),
+                    answer.getCreatedAt(),
+                    answer.getNumberOfLike(),
+                    answer.getNumberOfDislike(),
+                    answer.getUserId()));
+        }
+
+        return answerDTOS;
+    }
+
+    public AnswerDTO getAnswerById(int id) {
+        Answer answer = answerRepository.get(id);
+
+        return new AnswerDTO(
+                answer.getId(),
+                answer.getDescription(),
+                answer.getQuestionId(),
+                answer.getCreatedAt(),
+                answer.getNumberOfLike(),
+                answer.getNumberOfDislike(),
+                answer.getUserId());
+    }
+
+    public void deleteAnswerById(int id) {
+        answerRepository.delete(id);
+    }
+
+    public void addNewAnswer(NewAnswerDTO answer) {
+        answerRepository.add(answer.description(), answer.questionId(), LocalDateTime.now(), STARTER_NUMBER_OF_LIKES, STARTER_NUMBER_OF_DISLIKES, answer.userId());
+    }
+
+    public void updateAnswerById(int id, AnswerDTO answerDTO) {
+        answerRepository.update(id, answerDTO.description());
+    }
 }
