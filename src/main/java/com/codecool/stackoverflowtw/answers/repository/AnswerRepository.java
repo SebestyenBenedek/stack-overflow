19a44434e641588e4b21429564137bd006aa69c8
Bebe <125598346+SebestyenBenedek@users.noreply.github.com>
2023-10-18 16:32:34 +0200

    refactor AnswerRepository interface

--- src/main/java/com/codecool/stackoverflowtw/answers/repository/AnswerRepository.java
+++ src/main/java/com/codecool/stackoverflowtw/answers/repository/AnswerRepository.java
@@ -2,12 +2,13 @@ package com.codecool.stackoverflowtw.answers.repository;
 
 import com.codecool.stackoverflowtw.answers.model.Answer;
 
+import java.time.LocalDateTime;
 import java.util.Set;
 
 public interface AnswerRepository {
     Set<Answer> getAll(int id);
     Answer get(int id);
     void delete(int id);
-    void add(String description, int questionId);
+    void add(String description, int questionId, LocalDateTime createdAt, int numberOfLikes, int numberOfDislikes, int userId);
     void update(int id, String description);
 }
