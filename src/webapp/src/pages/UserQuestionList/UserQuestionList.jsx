7e32c0337a0f333179a3b89d7225107e4f351f43
Bebe <125598346+SebestyenBenedek@users.noreply.github.com>
2023-10-19 00:17:19 +0200

    implement UserQuestionList.jsx

---
+++ src/webapp/src/pages/UserQuestionList/UserQuestionList.jsx
@@ -0,0 +1,36 @@
+import {useEffect, useState} from "react";
+import question from "../../components/Question/Question";
+import Loading from "../../components/Loading/Loading";
+import QuestionList from "../../components/QuestionList/QuestionList";
+
+
+function UserQuestionList() {
+    const [loading, setLoading] = useState(true);
+    const [questionList, setQuestionList] = useState([]);
+
+    useEffect(() => {
+        const fetchQuestion = async () => {
+            const response = await fetch("/api/users/{id}/questions");
+            const jsonData = await response.json();
+            setQuestionList(jsonData.questions);
+            setLoading(false);
+        };
+        fetchQuestion();
+    }, []);
+
+    const handleDelete = async (id) => {
+        const response = await fetch(`/api/questions/${id}`, {
+            method: "DELETE",
+            headers: {
+                token: localStorage.getItem("token"),
+            },
+        });
+        setQuestionList(questionList.filter((question) => question.id !== id));
+    };
+
+    if (loading) return <Loading />;
+
+    return <QuestionTable questionList={questionList} onDelete={handleDelete} />;
+}
+
+export default UserQuestionList;
