04aaf498a94bd95faea3f56cbf21f0d5a6ae1dae
Bebe <125598346+SebestyenBenedek@users.noreply.github.com>
2023-10-18 22:49:10 +0200

    implement QuestionCreator.jsx

---
+++ src/webapp/src/pages/QuestionCreator/QuestionCreator.jsx
@@ -0,0 +1,29 @@
+import {useState} from "react";
+import Loading from "../../components/Loading";
+import {useNavigate} from "react-router-dom";
+import QuestionForm from "../../components/QuestionForm/QuestionForm";
+
+function QuestionCreator() {
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
+
+    const handleCreate = async (question) => {
+        setLoading(true);
+        await fetch('/api/questions/', {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json',
+                token: localStorage.getItem('token'),
+            },
+            body: JSON.stringify(question),
+        });
+        setLoading(false);
+        navigate('/api/questions/all');
+    };
+
+    if (loading) return <Loading />;
+
+    return <QuestionForm onCancel={() => navigate('/api/questions/all')} onSave={handleCreate()} />
+}
+
+export default QuestionCreator;
