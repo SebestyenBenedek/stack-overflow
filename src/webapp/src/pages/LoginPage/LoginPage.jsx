220d0b7175a9bd0bc91e3c0c0cca19cdfdf570ef
Balogh Barnabas <111227723+baloghb0110@users.noreply.github.com>
2023-10-18 15:10:05 +0200

    adding /login route and creating login form

--- src/webapp/src/pages/LoginPage/LoginPage.jsx
+++ src/webapp/src/pages/LoginPage/LoginPage.jsx
@@ -1,9 +1,34 @@
-import React from "react";
+import React, {useState} from "react";
 
 const LoginPage = () =>{
+    const [formInput, setFormInput] = useState({
+        username: "",
+        password: "",
+    });
+
+    const onInputChange = (e) => {
+        setFormInput((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+    };
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+
+    }
+
     return (
         <div>
-            login
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label>Username:</label>
+                    <input name="username" onChange={onInputChange} type="text"/>
+                </div>
+                <div>
+                    <label>Password:</label>
+                    <input name="password" onChange={onInputChange} type="password"/>
+                </div>
+
+                <button>Login</button>
+            </form>
         </div>
     );
 }
