db673e56464e8951f24da0143f1d3b5c3dcf0f27
Bebe <125598346+SebestyenBenedek@users.noreply.github.com>
2023-10-18 17:27:57 +0200

    add ComponentScan to Application

--- src/main/java/com/codecool/stackoverflowtw/StackoverflowTwApplication.java
+++ src/main/java/com/codecool/stackoverflowtw/StackoverflowTwApplication.java
@@ -2,9 +2,11 @@ package com.codecool.stackoverflowtw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
+@ComponentScan({"com.codecool.stackoverflowtw.logger"})
 public class StackoverflowTwApplication {
 
     public static void main(String[] args) {
