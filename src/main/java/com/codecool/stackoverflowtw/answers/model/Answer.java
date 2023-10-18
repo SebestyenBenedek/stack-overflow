ba77b15644d824c8a61a0f33d9c368f92511daf7
Bebe <125598346+SebestyenBenedek@users.noreply.github.com>
2023-10-18 16:32:02 +0200

    refactor Answer model class

--- src/main/java/com/codecool/stackoverflowtw/answers/model/Answer.java
+++ src/main/java/com/codecool/stackoverflowtw/answers/model/Answer.java
@@ -3,22 +3,22 @@ package com.codecool.stackoverflowtw.answers.model;
 import java.time.LocalDateTime;
 
 public class Answer {
-    private int counter;
-
     private final int id;
     private String description;
     private final int questionId;
-    private final LocalDateTime created;
-    private int numberOfLike;
-    private int numberOfDislike;
+    private final LocalDateTime createdAt;
+    private int numberOfLikes;
+    private int numberOfDislikes;
+    private final int userId;
 
-    public Answer(String description, int questionId) {
-        this.id = ++counter;
+    public Answer(int id, String description, int questionId, LocalDateTime createdAt, int numberOfLikes, int numberOfDislikes, int userId) {
+        this.id = id;
         this.description = description;
         this.questionId = questionId;
-        this.created = LocalDateTime.now();
-        this.numberOfLike = 0;
-        this.numberOfDislike = 0;
+        this.createdAt = createdAt;
+        this.numberOfLikes = numberOfLikes;
+        this.numberOfDislikes = numberOfDislikes;
+        this.userId = userId;
     }
 
     public int getId() {
@@ -33,15 +33,27 @@ public class Answer {
         return questionId;
     }
 
-    public LocalDateTime getCreated() {
-        return created;
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
     }
 
     public int getNumberOfLike() {
-        return numberOfLike;
+        return numberOfLikes;
     }
 
     public int getNumberOfDislike() {
-        return numberOfDislike;
+        return numberOfDislikes;
+    }
+
+    public int getNumberOfLikes() {
+        return numberOfLikes;
+    }
+
+    public int getNumberOfDislikes() {
+        return numberOfDislikes;
+    }
+
+    public int getUserId() {
+        return userId;
     }
 }
