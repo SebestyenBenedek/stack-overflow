3df4cd0e8f968fc0059f1a06b9368be001ae7e82
Bebe <125598346+SebestyenBenedek@users.noreply.github.com>
2023-10-19 17:14:39 +0200

    refactor AnswerRepositoryImpl class

--- src/main/java/com/codecool/stackoverflowtw/answers/repository/AnswerRepositoryImpl.java
+++ src/main/java/com/codecool/stackoverflowtw/answers/repository/AnswerRepositoryImpl.java
@@ -1,19 +1,22 @@
 package com.codecool.stackoverflowtw.answers.repository;
 
 import com.codecool.stackoverflowtw.answers.model.Answer;
+import com.codecool.stackoverflowtw.database.service.ConnectDatabase;
 import com.codecool.stackoverflowtw.database.service.ConnectDatabaseImpl;
 import com.codecool.stackoverflowtw.logger.Logger;
+import org.springframework.stereotype.Repository;
 
 import java.sql.*;
 import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
 
+@Repository
 public class AnswerRepositoryImpl implements AnswerRepository {
     private final Logger logger;
-    private ConnectDatabaseImpl connectDatabase;
+    private ConnectDatabase connectDatabase;
 
-    public AnswerRepositoryImpl(Logger logger, ConnectDatabaseImpl connectDatabase) {
+    public AnswerRepositoryImpl(Logger logger, ConnectDatabase connectDatabase) {
         this.logger = logger;
         this.connectDatabase = connectDatabase;
     }
@@ -28,26 +31,25 @@ public class AnswerRepositoryImpl implements AnswerRepository {
         String query = "SELECT * FROM answers WHERE questionId = ?";
 
         try (Connection conn = getConnection()) {
-            try (PreparedStatement preparedStatement = conn.prepareStatement(query)) {
-                preparedStatement.setInt(1, questionId);
-                ResultSet resultSet = preparedStatement.executeQuery();
-                while (resultSet.next()) {
-                    int id = resultSet.getInt("id");
-                    String description = resultSet.getString("description");
-                    int question = resultSet.getInt("questionId");
-                    LocalDateTime createdAt = resultSet.getTimestamp("createdAt").toLocalDateTime();
-                    int numberOfLikes = resultSet.getInt("numberOfLikes");
-                    int numberOfDislikes = resultSet.getInt("numberOfDislikes");
-                    int user = resultSet.getInt("userId");
+            PreparedStatement preparedStatement = conn.prepareStatement(query);
+            preparedStatement.setInt(1, questionId);
+            ResultSet resultSet = preparedStatement.executeQuery();
 
-                    answerList.add(new Answer(id, description, question, createdAt, numberOfLikes, numberOfDislikes, user));
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String description = resultSet.getString("description");
+                int question = resultSet.getInt("questionId");
+                LocalDateTime createdAt = resultSet.getTimestamp("createdAt").toLocalDateTime();
+                int numberOfLikes = resultSet.getInt("numberOfLikes");
+                int numberOfDislikes = resultSet.getInt("numberOfDislikes");
+                int user = resultSet.getInt("userId");
 
-                    logger.logInfo("Retrieving all the answers for the chosen question!");
-                }
-                resultSet.close();
-                preparedStatement.close();
-                conn.close();
+                answerList.add(new Answer(id, description, question, createdAt, numberOfLikes, numberOfDislikes, user));
+
             }
+            logger.logInfo("Retrieving all the answers for the chosen question!");
+            resultSet.close();
+            preparedStatement.close();
         } catch (SQLException e) {
             logger.logError("Error retrieving all the answers for the chosen question!" + e.getMessage());
         }
@@ -60,26 +62,25 @@ public class AnswerRepositoryImpl implements AnswerRepository {
         String query = "SELECT * FROM answers WHERE id = ?";
 
         try (Connection conn = getConnection()) {
-            try (PreparedStatement preparedStatement = conn.prepareStatement(query)) {
-                preparedStatement.setInt(1, answerId);
-                ResultSet resultSet = preparedStatement.executeQuery();
-                if (resultSet.next()) {
-                    int id = resultSet.getInt("id");
-                    String description = resultSet.getString("description");
-                    int question = resultSet.getInt("questionId");
-                    LocalDateTime createdAt = resultSet.getTimestamp("createdAt").toLocalDateTime();
-                    int numberOfLikes = resultSet.getInt("numberOfLikes");
-                    int numberOfDislikes = resultSet.getInt("numberOfDislikes");
-                    int user = resultSet.getInt("userId");
+            PreparedStatement preparedStatement = conn.prepareStatement(query);
+            preparedStatement.setInt(1, answerId);
+            ResultSet resultSet = preparedStatement.executeQuery();
 
-                    logger.logInfo("Retrieving answer was successfully!");
+            if (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String description = resultSet.getString("description");
+                int question = resultSet.getInt("questionId");
+                LocalDateTime createdAt = resultSet.getTimestamp("createdAt").toLocalDateTime();
+                int numberOfLikes = resultSet.getInt("numberOfLikes");
+                int numberOfDislikes = resultSet.getInt("numberOfDislikes");
+                int user = resultSet.getInt("userId");
 
-                    return new Answer(id, description, question, createdAt, numberOfLikes, numberOfDislikes, user);
-                }
-                resultSet.close();
-                preparedStatement.close();
-                conn.close();
+                logger.logInfo("Retrieving answer was successfully!");
+
+                return new Answer(id, description, question, createdAt, numberOfLikes, numberOfDislikes, user);
             }
+            resultSet.close();
+            preparedStatement.close();
         } catch (SQLException e) {
             logger.logError("Error retrieving answer: " + e.getMessage());
         }
@@ -91,14 +92,13 @@ public class AnswerRepositoryImpl implements AnswerRepository {
         String query = "DELETE FROM answers WHERE id = ?";
 
         try (Connection conn = getConnection()) {
-            try (PreparedStatement preparedStatement = conn.prepareStatement(query)) {
-                preparedStatement.setInt(1, id);
-                preparedStatement.executeUpdate();
+            PreparedStatement preparedStatement = conn.prepareStatement(query);
+            preparedStatement.setInt(1, id);
+            preparedStatement.executeUpdate();
 
-                logger.logInfo("Answer deleted successfully!");
-                preparedStatement.close();
-                conn.close();
-            }
+            logger.logInfo("Answer deleted successfully!");
+
+            preparedStatement.close();
         } catch (SQLException e) {
             logger.logError("Error deleting answer: " + e.getMessage());
         }
@@ -109,18 +109,17 @@ public class AnswerRepositoryImpl implements AnswerRepository {
         String query = "INSERT INTO answers(description, questionId, createdAt, numberOfLikes, numberOdDislikes, userId) VALUES(?,?,?,?,?,?)";
 
         try (Connection conn = getConnection()) {
-            try (PreparedStatement preparedStatement = conn.prepareStatement(query)) {
-                preparedStatement.setString(1, description);
-                preparedStatement.setInt(2, questionId);
-                preparedStatement.setTimestamp(3, Timestamp.valueOf(createdAt));
-                preparedStatement.setInt(4, numberOfLikes);
-                preparedStatement.setInt(5, numberOfDislikes);
-                preparedStatement.setInt(6, userId);
+            PreparedStatement preparedStatement = conn.prepareStatement(query);
+            preparedStatement.setString(1, description);
+            preparedStatement.setInt(2, questionId);
+            preparedStatement.setTimestamp(3, Timestamp.valueOf(createdAt));
+            preparedStatement.setInt(4, numberOfLikes);
+            preparedStatement.setInt(5, numberOfDislikes);
+            preparedStatement.setInt(6, userId);
 
-                logger.logInfo("Adding a new answer to the chosen question was successfully!");
-                preparedStatement.close();
-                conn.close();
-            }
+            logger.logInfo("Adding a new answer to the chosen question was successfully!");
+
+            preparedStatement.close();
         } catch (SQLException e) {
             logger.logError("Error adding new answer: " + e.getMessage());
         }
@@ -128,17 +127,16 @@ public class AnswerRepositoryImpl implements AnswerRepository {
 
     @Override
     public void update(int id, String description) {
-        String query = "UPDATE anwers SET description = ? WHERE id = ?";
+        String query = "UPDATE answers SET description = ? WHERE id = ?";
 
         try (Connection conn = getConnection()) {
-            try (PreparedStatement preparedStatement = conn.prepareStatement(query)) {
-                preparedStatement.setString(1, description);
-                preparedStatement.setInt(2, id);
+            PreparedStatement preparedStatement = conn.prepareStatement(query);
+            preparedStatement.setString(1, description);
+            preparedStatement.setInt(2, id);
 
-                logger.logInfo("Updating answer was successfully!");
-                preparedStatement.close();
-                conn.close();
-            }
+            logger.logInfo("Updating answer was successfully!");
+
+            preparedStatement.close();
         } catch (SQLException e) {
             logger.logError("Error updating answer: " + e.getMessage());
         }
