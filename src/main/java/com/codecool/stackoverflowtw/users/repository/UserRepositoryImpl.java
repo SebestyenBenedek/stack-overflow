7730bfe517030c0f41a90cd33191728ee7e86ada
Bebe <125598346+SebestyenBenedek@users.noreply.github.com>
2023-10-19 17:14:13 +0200

    refactor UserRepositoryImpl class

--- src/main/java/com/codecool/stackoverflowtw/users/repository/UserRepositoryImpl.java
+++ src/main/java/com/codecool/stackoverflowtw/users/repository/UserRepositoryImpl.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 import java.sql.*;
 import java.util.HashSet;
 import java.util.Set;
+
 @Repository
 public class UserRepositoryImpl implements UserRepository {
     private final Logger logger;
@@ -31,25 +32,24 @@ public class UserRepositoryImpl implements UserRepository {
         String query = "SELECT * FROM users";
 
         try (Connection conn = getConnection()) {
-            PreparedStatement stmt = conn.prepareStatement(query);
-            ResultSet resultSet = stmt.executeQuery();
+            PreparedStatement preparedStatement = conn.prepareStatement(query);
+            ResultSet resultSet = preparedStatement.executeQuery();
 
-                    while (resultSet.next()) {
-                        int id = resultSet.getInt("id");
-                        String username = resultSet.getString("username");
-                        String password = resultSet.getString("password");
-                        String email = resultSet.getString("email");
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String username = resultSet.getString("username");
+                String password = resultSet.getString("password");
+                String email = resultSet.getString("email");
 
-                        users.add(new User(id, username, password, email));
+                users.add(new User(id, username, password, email));
 
-                        logger.logInfo("Retrieving all the Users was successfully!");
-                    }
-                    resultSet.close();
-                    stmt.close();
-
-            } catch (SQLException e) {
-                logger.logError("Error retrieving all the Users " + e.getMessage());
             }
+            logger.logInfo("Retrieving all the Users was successfully!");
+            resultSet.close();
+            preparedStatement.close();
+        } catch (SQLException e) {
+            logger.logError("Error retrieving all the Users " + e.getMessage());
+        }
 
         return users;
     }
@@ -59,25 +59,24 @@ public class UserRepositoryImpl implements UserRepository {
         String query = "SELECT * FROM users WHERE id = ?";
 
         try (Connection conn = getConnection()) {
-            try (PreparedStatement preparedStatement = conn.prepareStatement(query)) {
-                preparedStatement.setObject(1, id);
-                ResultSet resultSet = preparedStatement.executeQuery();
-                if (resultSet.next()) {
-                    int userId = resultSet.getInt("id");
-                    String username = resultSet.getString("username");
-                    String password = resultSet.getString("password");
-                    String email = resultSet.getString("email");
+            PreparedStatement preparedStatement = conn.prepareStatement(query);
+            preparedStatement.setObject(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
 
-                    logger.logInfo("Retrieving Uer was successfully!");
+            if (resultSet.next()) {
+                int userId = resultSet.getInt("id");
+                String username = resultSet.getString("username");
+                String password = resultSet.getString("password");
+                String email = resultSet.getString("email");
 
-                    return new User(userId, username, password, email);
-                }
-                resultSet.close();
-                preparedStatement.close();
-                conn.close();
+                logger.logInfo("Retrieving User was successfully!");
+
+                return new User(userId, username, password, email);
             }
+            resultSet.close();
+            preparedStatement.close();
         } catch (SQLException e) {
-            logger.logError("Error retrieving question: " + e.getMessage());
+            logger.logError("Error retrieving User: " + e.getMessage());
         }
         return null;
     }
@@ -85,52 +84,29 @@ public class UserRepositoryImpl implements UserRepository {
 
     @Override
     public User findByUsername(String name) {
-        try {
-            Connection conn = getConnection();
-            String query = "SELECT * FROM users WHERE username = ?";
-            PreparedStatement ps = conn.prepareStatement(query);
+        String query = "SELECT * FROM users WHERE username = ?";
 
-            ps.setString(1, name);
+        try (Connection conn = getConnection()) {
+            ;
+            PreparedStatement preparedStatement = conn.prepareStatement(query);
+            preparedStatement.setString(1, name);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String username = resultSet.getString("username");
+                String password = resultSet.getString("password");
+                String email = resultSet.getString("email");
 
-            ResultSet rs = ps.executeQuery();
-
-            while(rs.next()){
-                int id = rs.getInt("id");
-                String username = rs.getString("username");
-                String password = rs.getString("password");
-                String email = rs.getString("email");
+                logger.logInfo("Retrieving User by name was successfully!");
 
                 return new User(id, username, password, email);
             }
-
-        }catch (SQLException e){
+            resultSet.close();
+            preparedStatement.close();
+        } catch (SQLException e) {
             logger.logInfo(e.getMessage());
         }
 
-       /* String query = "SELECT * FROM users WHERE username = ?";
-        System.out.println(name);
-        try (Connection conn = getConnection()) {
-            try (PreparedStatement preparedStatement = conn.prepareStatement(query)) {
-                preparedStatement.setObject(1, name);
-                ResultSet resultSet = preparedStatement.executeQuery();
-                System.out.println(resultSet.toString());
-                if (resultSet.next()) {
-                    int id = resultSet.getInt("id");
-                    String username = resultSet.getString("username");
-                    String password = resultSet.getString("password");
-                    String email = resultSet.getString("email");
-
-                    logger.logInfo("Retrieving Uer was successfully!");
-
-                    return new User(id, username, password, email);
-                }
-                resultSet.close();
-                preparedStatement.close();
-                conn.close();
-            }
-        } catch (SQLException e) {
-            logger.logError("Error retrieving question: " + e.getMessage());
-        }*/
         return null;
     }
 
@@ -139,14 +115,13 @@ public class UserRepositoryImpl implements UserRepository {
         String query = "DELETE FROM users WHERE id = ?";
 
         try (Connection conn = getConnection()) {
-            try (PreparedStatement preparedStatement = conn.prepareStatement(query)) {
-                preparedStatement.setObject(1, id);
-                preparedStatement.executeUpdate();
+            PreparedStatement preparedStatement = conn.prepareStatement(query);
+            preparedStatement.setObject(1, id);
+            preparedStatement.executeUpdate();
 
-                logger.logInfo("Question deleted successfully!");
-                preparedStatement.close();
-                conn.close();
-            }
+            logger.logInfo("Question deleted successfully!");
+
+            preparedStatement.close();
         } catch (SQLException e) {
             logger.logError("Error deleting question: " + e.getMessage());
         }
@@ -157,15 +132,14 @@ public class UserRepositoryImpl implements UserRepository {
         String query = "INSERT INTO users(username, password, email) VALUES(?,?,?)";
 
         try (Connection conn = getConnection()) {
-            try (PreparedStatement preparedStatement = conn.prepareStatement(query)) {
-                preparedStatement.setString(1, username);
-                preparedStatement.setString(2, password);
-                preparedStatement.setString(3, email);
+            PreparedStatement preparedStatement = conn.prepareStatement(query);
+            preparedStatement.setString(1, username);
+            preparedStatement.setString(2, password);
+            preparedStatement.setString(3, email);
 
-                logger.logInfo("Adding a new User was successfully!");
-                preparedStatement.close();
-                conn.close();
-            }
+            logger.logInfo("Adding a new User was successfully!");
+
+            preparedStatement.close();
         } catch (SQLException e) {
             logger.logError("Error adding new User: " + e.getMessage());
         }
@@ -176,16 +150,15 @@ public class UserRepositoryImpl implements UserRepository {
         String query = "UPDATE users SET username = ?, password = ?, email = ? WHERE id = ?";
 
         try (Connection conn = getConnection()) {
-            try (PreparedStatement preparedStatement = conn.prepareStatement(query)) {
-                preparedStatement.setString(1, username);
-                preparedStatement.setString(2, password);
-                preparedStatement.setString(3, email);
-                preparedStatement.setInt(4, id);
+            PreparedStatement preparedStatement = conn.prepareStatement(query);
+            preparedStatement.setString(1, username);
+            preparedStatement.setString(2, password);
+            preparedStatement.setString(3, email);
+            preparedStatement.setInt(4, id);
 
-                logger.logInfo("Updating User was successfully!");
-                preparedStatement.close();
-                conn.close();
-            }
+            logger.logInfo("Updating User was successfully!");
+
+            preparedStatement.close();
         } catch (SQLException e) {
             logger.logError("Error updating User: " + e.getMessage());
         }
