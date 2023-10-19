c2bf19055b83c743ac51031d910641ccd745937d
Lajos Savoly <125366427+SavolyLali@users.noreply.github.com>
2023-10-19 10:02:13 +0200

    start creating RegistrationPage.jsx

---
+++ src/webapp/src/pages/RegistrationPage/RegistrationPage.jsx
@@ -0,0 +1,9 @@
+import React, {useState} from "react";
+
+const RegistrationPage = () => {
+    const [formInput, setFormInput] = useState({
+        username: "",
+        password: "",
+        email: ""
+    });
+}
