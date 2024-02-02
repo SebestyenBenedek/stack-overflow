2a8ef7d2b3addf50529951ac6dbd8917f158d5d4
SebestyenBenedek <sebi.bebe@gmail.com>
2024-02-02 15:24:34 +0100

    refactor StackoverflowTwApplication

--- src/main/java/com/codecool/stackoverflowtw/StackoverflowTwApplication.java
+++ src/main/java/com/codecool/stackoverflowtw/StackoverflowTwApplication.java
@@ -4,14 +4,9 @@ import com.codecool.stackoverflowtw.database.service.ConnectDatabase;
 import com.codecool.stackoverflowtw.database.service.ConnectDatabaseImpl;
 import com.codecool.stackoverflowtw.logger.ConsoleLogger;
 import com.codecool.stackoverflowtw.logger.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-
-import java.sql.Connection;
-
 
 @SpringBootApplication
 public class StackoverflowTwApplication {
