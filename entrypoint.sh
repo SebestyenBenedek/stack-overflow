a2ee169e9394860327303c8fe00c2ed1bdae7f31
SebestyenBenedek <sebi.bebe@gmail.com>
2024-02-02 15:24:02 +0100

    add arguments to entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,16 @@
 
 # Check if the .env file exists
 if [ ! -f .env ]; then
+  # Check if all required arguments are provided
+    if [ $# -ne 3 ]; then
+      echo "Usage: $0 <username> <password> <database>"
+      exit 1
+    fi
+
   # Create the .env file with the necessary credentials
-  echo "POSTGRES_DB=${POSTGRES_DB}" > .env
-  echo "POSTGRES_USER=${POSTGRES_USER}" >> .env
-  echo "POSTGRES_PASSWORD=${POSTGRES_PASSWORD}" >> .env
+  echo "POSTGRES_DB=$3" > .env
+  echo "POSTGRES_USER=$1" >> .env
+  echo "POSTGRES_PASSWORD=$2" >> .env
 fi
 
 # Start your Spring Boot application
